6f6af04b6160da75a30608f972be0afd5aa2886b
janis-jenny <pcardenasvm@gmail.com>
2020-10-27 11:51:21 -0500

    test my_any, my_none and my_count

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,6 +2,7 @@ require './enumerables'
 
 describe Enumerable do
   let(:arr) { [1, 2, 3, 4] }
+  let(:arr1) { [1, 2, 3, nil] }
   let(:ans) { [] }
   let(:bool) { [true, false, nil] }
   let(:str) { %w[Dog Cat Mouse] }
@@ -56,5 +57,57 @@ describe Enumerable do
     it "returns true if no block is given" do
       expect(arr.my_all?).to eql(true)
     end
+
+    it "returns false if there is a false element" do
+      expect(arr1.my_all?(Numeric)).to eql(false)
+    end
   end
+
+  describe '#my_any?' do
+
+    it "my_any returns true if there is one elemente that meets the condition" do
+      expect(str.my_any? { |word| word.length >= 3 }).to be true 
+    end
+    
+    it "my_any returns false if there isn't one elemente that meets the condition" do
+      expect(str.my_any? { |word| word.length >= 10 }).to be false 
+    end
+
+    it "my_any returns true if there is no block given " do
+      expect(arr.my_any?(Numeric)).to be true 
+    end
+
+  end
+
+  describe '#my_none?' do
+
+    it "my_none returns true if there is one element that no meets the condition" do
+      expect(str.my_none? { |word| word.length == 2 }).to be true 
+    end
+    
+    it "my_none returns false if there is one elemente that meets the condition" do
+      expect(str.my_none? { |word| word.length >= 3 }).to be false 
+    end
+
+    it "my_none returns false if there is no block given " do
+      expect(arr1.my_none?(Numeric)).to be false 
+    end
+
+  end
+
+  describe '#my_count?' do
+
+    it "my_count will count the elements that meet the condition in a block given" do
+      expect(arr.my_count { |n| n }).to eql(4) 
+    end
+    
+    it "my_count will count the elements that meet the condition in a block given" do
+      expect(arr.my_count { |n| n%2==0 }).to eql(2)
+    end
+
+    it "if no block is given returns the length of an array" do
+      expect(arr.my_count).to eql(arr.length)
+    end
+  end
+
 end
